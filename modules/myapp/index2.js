9b055ad1c59262c45c4e1b683ab992428671e985
SHjalilo <aaaabdjalil@gmail.com>
2023-05-21 18:37:11 +0100

    first commit !

---
+++ modules/myapp/index2.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+var myPath = path.dirname('/home/abdjalil/Desktop/node.js/myapp/index2.js');
+
+console.log(myPath);
+
+// ----------------- //
+console.log("--------------");
+
+
+var dt = require('./test');
+const { log } = require('console');
+
+var mydt = dt.myDate();
+
+console.log(mydt);
